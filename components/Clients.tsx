ed2f3f1289ca888bcfb505040ddfe31509f1e6b7
faye092 <liaofei167@gmail.com>
2024-11-05 16:31:22 +0100

    update the satisfied testimonials section

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { TypewriterEffectSmooth } from './ui/typewriter-effect';
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards';
+import { companies, testimonials } from '@/data';
 
 function Clients() {
     const words = [
@@ -8,7 +10,7 @@ function Clients() {
             className:"text-white dark:text-white"
         },
         {
-            text:"clients.",
+            text:"testimonials.",
             className:"text-purple dark:text-purple"
         },
     ];
@@ -17,6 +19,30 @@ function Clients() {
     <div className='py-20' id="projects">
       <div className='flex flex-col items-center justify-center'>
         <TypewriterEffectSmooth words={words} />
+      </div>
+      <div className='flex flex-col items-center max-lg:mt-10'>       
+        <InfiniteMovingCards 
+            items={testimonials}
+            direction='right'
+            speed='slow'
+        />
+        
+        <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
+            {companies.map(({id,img,name,nameImg}) => (
+                <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
+                    <img 
+                        src={img}
+                        alt={name}
+                        className='md:w-10 w-5'
+                    />
+                    <img 
+                        src={nameImg}
+                        alt={name}
+                        className='md:w-24 w-20'
+                    />
+                </div>
+            ))}
+        </div>
       </div>
     </div>
   )
--- data/index.ts
+++ data/index.ts
@@ -108,31 +108,31 @@ export const navItems = [
   export const testimonials = [
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Exceeded expectations with innovative designs. A true asset for any frontend development project.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Reliable, deadline-oriented developer. Crafted interactive designs that truly engage and captivate users.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Effortlessly blended creativity with functionality. Played a pivotal role in our app's success.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Faye was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "Collaborating with Faye was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
       name: "Michael Johnson",
       title: "Director of AlphaStream Technologies",
     },
