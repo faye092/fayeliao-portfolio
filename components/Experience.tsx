cafd69fa34de4baedc0104b48be235d8392ef8ca
faye092 <liaofei167@gmail.com>
2024-11-05 16:55:13 +0100

    update experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { TypewriterEffectSmooth } from './ui/typewriter-effect';
 
-const Experience = () => {
+function Experience() {
+  const words = [
+      {
+          text:"My",
+          className:"text-white dark:text-white"
+      },
+      {
+          text:"work experience.",
+          className:"text-purple dark:text-purple"
+      },
+  ];
+    
   return (
-    <div>
-      Experience
-    </div>
+    <div className='py-20' id="projects">
+      <div className='flex flex-col items-center justify-center'>
+        <TypewriterEffectSmooth words={words} />
+      </div>
+      </div>
   )
 }
 
